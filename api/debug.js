7920b128ebfc4b397b25de773640545835e8e376
Samay <samaanya1@gmail.com>
2026-05-26 08:35:14 +0530

    Remove debug endpoint

--- api/debug.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    key_id: process.env.RAZORPAY_KEY_ID ?? "NOT SET",
-    has_key_secret: !!process.env.RAZORPAY_KEY_SECRET,
-    key_secret_length: process.env.RAZORPAY_KEY_SECRET?.length ?? 0,
-  });
-}
